c062d17cb312bb98232d2b7e5f09e8e1ddff66f1
KenanYusubov <kanan.yusubov.1999@gmail.com>
2020-07-18 13:19:21 -0700

    theme cubit is completed

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020, Kanan Yusubov. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential
+ * Written by: Kanan Yusubov <kanan.yusubov@yandex.com>, July 2020
+ */
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cubit/flutter_cubit.dart';
--- lib/presentation/shared/app_text_styles.dart
+++ lib/presentation/shared/app_text_styles.dart
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020, Kanan Yusubov. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential
+ * Written by: Kanan Yusubov <kanan.yusubov@yandex.com>, July 2020
+ */
+
 import 'package:flutter/material.dart';
 import '../../utils/responsive_helper/size_config.dart';
 import './app_colors.dart';
