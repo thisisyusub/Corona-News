6389ebf0d863c208c085ca926d0552609f69bdbc
KenanYusubov <kanan.yusubov.1999@gmail.com>
2020-07-28 18:29:52 +0400

    import problem solved

--- lib/presentation/pages/generic_page/generic_page.dart
+++ lib/presentation/pages/generic_page/generic_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../widgets/custom_appbar.dart';
 import '../../widgets/states/sliver_states.dart';
